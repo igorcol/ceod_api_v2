db3d756b6d9689ce49fc630838bd3e159028c09c
Igor - NOTE_2 <igor.colombini@gmail.com>
2025-03-05 23:04:07 -0300

    feat(UpdateEmailReceived)

---
+++ src/server/database/providers/user/UpdateEmailReceived.ts
@@ -0,0 +1,26 @@
+import User from "../../../models/userModel";
+
+interface UpdateEmailReceivedBod {
+    emailReceived: boolean
+}
+
+
+export const updateEmailReceived = async (ID: string, body: UpdateEmailReceivedBod) => {
+    try {
+        const user = await User.findOneAndUpdate(
+            { _id: ID },
+            { emailReceived: body.emailReceived },
+            { new: true, upsert: true }
+        );
+        if (user === null) {
+            console.log(`❌ Não existe nenhum usuário com o id ${ID}`)
+            return new Error(`❌ Não existe nenhum usuário com o id ${ID}`)
+        }
+        console.log(`✅ Email recebido -> id: ${ID}`)
+        return user
+    } 
+    catch (err) {
+        console.log('❌ Erro ao atualizar emailReceived', err)
+        return new Error('❌ Erro ao atualizar emailReceived')
+    }
+}
