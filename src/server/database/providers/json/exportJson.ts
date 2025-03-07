243da5098cf066bfb03554d50361d11a84769bd6
igorcol <igor.colombini@gmail.com>
2025-03-07 18:12:37 -0300

    refact(exportJson): Create EMAIL, PRESENCA and emailReceived fields when exporting json to db

--- src/server/database/providers/json/exportJson.ts
+++ src/server/database/providers/json/exportJson.ts
@@ -1,16 +1,34 @@
 import fs from 'fs'
 import User from '../../../models/userModel';
 
+
+interface UserJson {
+    EMAIL?: string;
+    presenca?: boolean;
+    emailReceived?: boolean;
+    [key: string]: any; // Permite outros campos sem erro
+}
+
+
 export const exportJson = async (json_path: string) => {
+
     try {
         const rawData = fs.readFileSync(json_path, 'utf8');
         const data = JSON.parse(rawData);
 
         if (!data.users || !Array.isArray(data.users)) {
+            console.log('❌ Formato de arquivo inválido' )
             return new Error('❌ Formato de arquivo inválido' );
         }
 
-        await User.insertMany(data.users);
+        const userToInsert = data.users.map((user: UserJson) => ({
+            ...user,
+            EMAIL: user.EMAIL ?? null,
+            presenca: user.presenca ?? false,
+            emailReceived: user.emailReceived ?? false
+        }))
+
+        await User.insertMany(userToInsert);
         console.log("🟩 JSON EXPORTED")
         return 
     } 
