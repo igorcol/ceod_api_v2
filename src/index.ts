215045b9d0fe839a6c3ede6a9787778311bd2ffb
Igor - NOTE_2 <igor.colombini@gmail.com>
2025-03-09 13:13:35 -0300

    Json_Name print on startup

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import { server } from "./server/Server";
 
 const PORT = config.PORT
 const ENV = config.ENV
+const json = process.env.JSON_NAME
 
 server.listen(PORT, () => {
     console.log(`
@@ -11,6 +12,7 @@ server.listen(PORT, () => {
     ⚡ Port: ${PORT}        
     ⚡ Env: ${ENV}     
     ===========================
+    Json: ${json}
     `);
 })
 
