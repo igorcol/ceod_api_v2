8912a81fca899affc741b2b776941e7b7bd8d5ae
igorcol <igor.colombini@gmail.com>
2025-03-10 21:45:22 -0300

    refact(GetEmails): +emailReceived value

--- src/server/database/providers/user/GetEmails.ts
+++ src/server/database/providers/user/GetEmails.ts
@@ -5,14 +5,19 @@ import User from "../../../models/userModel";
 export const getEmails = async () => {
     let EMAILS: Array<object> = []
     try {
-        const emails: Array<object> = await User.find({}, 'EMAIL');
+        const emails: Array<{ _id: string, EMAIL: string, emailReceived?: boolean }> = await User.find({}, 'EMAIL emailReceived');
 
         if (!emails || emails.length === 0) {
             throw new Error('Nenhum email encontrado');
         }
 
         console.log(`🟢 USER | Get Emails -> ${emails.length} emails found. \n`)
-        emails.forEach(email => EMAILS.push(email));
+        //emails.forEach(email => EMAILS.push(email));
+        EMAILS = emails.map(email => ({
+            _id: email._id,
+            EMAIL: email.EMAIL,
+            emailReceived: email.emailReceived || false
+        }))
         return EMAILS
     }
     catch (error) {
