fe746b708687a3fc39167f3293903585f8e73fdd
Igor - NOTE_2 <igor.colombini@gmail.com>
2025-05-13 09:59:12 -0300

    refact(getEmail)

--- src/server/database/providers/user/GetEmails.ts
+++ src/server/database/providers/user/GetEmails.ts
@@ -1,23 +1,39 @@
-import { Request, Response } from "express";
+import { databases } from "../../../services/appwrite.service";
+import { Inscrito } from "../../../../../types/inscritos";
+import { Query } from "node-appwrite";
+import { config } from "../../../config";
 
-import User from "../../../models/userModel";
+const DB_ID = config.appwrite.databaseId;
+const COLLECTION_ID = config.appwrite.inscritosCollectionId;
 
-export const getEmails = async () => {
-    let EMAILS: Array<object> = []
+export const getEmails = async (): Promise<
+Array<{
+    $id: string;
+    CODIGO: string;
+    NOME: string;
+    EMAIL: string;
+    emailReceived: boolean;
+}> | Error> => {
     try {
-        const emails: Array<{ _id: string, EMAIL: string, emailReceived?: boolean }> = await User.find({}, 'EMAIL emailReceived');
+        const result = await databases.listDocuments<Inscrito>(
+            DB_ID, COLLECTION_ID,
+            [Query.limit(5000)]
+        );
 
-        if (!emails || emails.length === 0) {
-            throw new Error('Nenhum email encontrado');
+        if (!result || result.documents.length === 0) {
+            throw new Error("Nenhum email encontrado");
         }
 
-        console.log(`🟢 USER | Get Emails -> ${emails.length} emails found. \n`)
-        //emails.forEach(email => EMAILS.push(email));
-        EMAILS = emails.map(email => ({
-            _id: email._id,
-            EMAIL: email.EMAIL,
-            emailReceived: email.emailReceived || false
-        }))
+        console.log(`🟢 USER | Get Emails -> ${result.documents.length} emails found.\n`);
+
+        const EMAILS = result.documents.map(doc => ({
+            $id: doc.$id,
+            CODIGO: doc.CODIGO,
+            NOME: doc.NOME,
+            EMAIL: doc.EMAIL,
+            emailReceived: doc.emailReceived ?? false
+        }));
+
         return EMAILS
     }
     catch (error) {
