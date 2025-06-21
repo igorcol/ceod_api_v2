320d3d3de09806feb1e54095b2e930cdda18f407
Igor - NOTE_2 <igor.colombini@gmail.com>
2025-06-21 02:38:37 -0300

    feat: Melhora e flexibiliza a busca de usuários

--- src/server/database/providers/user/Search.ts
+++ src/server/database/providers/user/Search.ts
@@ -3,41 +3,39 @@ import { Inscrito } from "../../../../../types/inscritos";
 import { config } from "../../../config";
 import { databases } from "../../../services/appwrite.service";
 
-
 const DB_ID = config.appwrite.databaseId;
 const COLLECTION_ID = config.appwrite.inscritosCollectionId;
 
-
-// Função auxiliar
 const isID = (input: string): boolean => {
     return /^\d+$/.test(input.trim());
 };
 
-
 export const search = async (searchTerm: string): Promise<Inscrito[] | Error> => {
     try {
-        let attributeToSearch: string;
+        let queries: string[];
 
         if (isID(searchTerm)) {
-            attributeToSearch = 'ID_SISDM';
             console.log(`🔎 Termo de busca identificado como ID. Buscando em [ID_SISDM] por: "${searchTerm}"`);
+            queries = [Query.search('ID_SISDM', searchTerm)];
         } else {
-            attributeToSearch = 'NOME';
-            console.log(`🔎 Termo de busca identificado como NOME. Buscando em [NOME] por: "${searchTerm}"`);
-        }
+            console.log(`🔎 Termo de busca identificado como NOME. Buscando por TODOS os termos: "${searchTerm}"`);
+            
+            // Quebra o termo de busca em palavras individuais
+            const words = searchTerm.trim().split(' ').filter(word => word.length > 0);
 
-        const searchQuery = Query.search(attributeToSearch, searchTerm);
+            // Array de queries, uma para cada palavra.
+            queries = words.map(word => Query.search('NOME', word));
+        }
 
         const results = await databases.listDocuments<Inscrito>(
             DB_ID, COLLECTION_ID,
-            [searchQuery]
+            queries 
         );
 
-        console.log(`🟩 USERS | Search -> Encontrados ${results.total} resultados para "${searchTerm}" em [${attributeToSearch}]`);
+        console.log(`🟩 USERS | Search -> Encontrados ${results.total} resultados para "${searchTerm}"`);
         return results.documents;
 
-    }
-    catch (error) {
+    } catch (error) {
         console.error('❌ Erro ao executar a busca de usuários:', error);
         return new Error(`❌ Erro ao executar a busca de usuários: ${error}`);
     }
